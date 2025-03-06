14244310a10981d0253154f9c05dd785d77aea18
AnnaKarolynapditabira <anna.paula@pditabira.com>
2025-03-06 15:11:09 -0300

    Add files via upload

---
+++ Módulo 2/aula2_questao2.py
@@ -0,0 +1,11 @@
+texto = "O resultado é"
+valor1 = 10
+valor2 = 3.5
+
+soma = valor1 + valor2
+
+print(texto, valor1, valor2, soma)
+print("Tipo de 'texto':", type(texto))
+print("Tipo de 'valor1':", type(valor1))
+print("Tipo de 'valor2':", type(valor2))
+print("Tipo de 'soma':", type(soma))
---
+++ Módulo 2/aula2_questao4.py
@@ -0,0 +1,9 @@
+
+saldo = 500.0
+juros = 1.01
+
+saldo *= juros  # 1º mês
+saldo *= juros  # 2º mês
+saldo *= juros  # 3º mês
+
+print("Após 3 meses meu novo saldo é:", saldo)
